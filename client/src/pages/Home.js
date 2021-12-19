029e843467be9ce898eb16394aab4b13f2edc9ac
e1m3m0 <gamateo@gmail.com>
2021-12-18 23:11:11 -0500

    add thought textarea

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,6 +4,8 @@ import ThoughtList from '../components/ThoughtList';
 import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
 import Auth from '../utils/auth';
 import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
+
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_THOUGHTS);
@@ -13,26 +15,34 @@ const Home = () => {
   const thoughts = data?.thoughts || [];
 
   const loggedIn = Auth.loggedIn();
-  
+
   return (
     <main>
-      <div className='flex-row justify-space-between'>
-        <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
+      <div className="flex-row justify-space-between">
+        {loggedIn && (
+          <div className="col-12 mb-3">
+            <ThoughtForm />
+          </div>
+        )}
+        <div className={`col-12 mb-3 ${loggedIn && "col-lg-8"}`}>
           {loading ? (
             <div>Loading...</div>
           ) : (
-            <ThoughtList thoughts={thoughts} title="Some Feed For Thought(s)..." />          
+            <ThoughtList
+              thoughts={thoughts}
+              title="Some Feed For Thought(s)..."
+            />
           )}
         </div>
         {loggedIn && userData ? (
-              <div className='col-12 col-lg-3 mb-3'>
-                <FriendList
-                username={userData.me.username}
-                friendCount={userData.me.friendCount}
-                friends={userData.me.friends}
-                />
-                </div>
-            ) : null}
+          <div className="col-12 col-lg-3 mb-3">
+            <FriendList
+              username={userData.me.username}
+              friendCount={userData.me.friendCount}
+              friends={userData.me.friends}
+            />
+          </div>
+        ) : null}
       </div>
     </main>
   );
