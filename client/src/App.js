e10cbd2915b739b41e463e5fcd341f187d1e432d
e1m3m0 <gamateo@gmail.com>
2021-12-18 22:27:55 -0500

    jwt token validation and header incorporation

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,5 @@
-import {
-  ApolloClient,
-  ApolloProvider,
-  createHttpLink,
-  InMemoryCache,
-} from "@apollo/client";
+import {ApolloClient, ApolloProvider, createHttpLink, InMemoryCache,} from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import React from "react";
 import Footer from "./components/Footer";
@@ -20,8 +16,18 @@ function App() {
     uri: "/graphql",
   });
 
+  const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('id_token');
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    };
+  });
+
   const client = new ApolloClient({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
   });
 
