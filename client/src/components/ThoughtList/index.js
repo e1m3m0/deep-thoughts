dd2f0aad6d4a092e408b51c4ff8609581abb5b34
e1m3m0 <gamateo@gmail.com>
2021-12-18 20:14:16 -0500

    pages completed and linked

--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ThoughtList = ({ thoughts, title }) => {
   if (!thoughts.length) {
@@ -8,20 +9,30 @@ const ThoughtList = ({ thoughts, title }) => {
   return (
     <div>
       <h3>{title}</h3>
-      {thoughts && thoughts.map(thought => (
-        <div key={thought.id} className="card mb-3">
-          <p className="card-header">
-            {thought.username}
-            thought on {thought.createdAt}
-          </p>
-          <div className="card0-body">
-            <p>{thought.thoughtText}</p>
-            <p className="mb-0">
-              Reactions: {thought.reactionsCount} || Click to{' '}{thought.reactionsCount ? 'see' : 'start'} the discussion!
+      {thoughts &&
+        thoughts.map((thought) => (
+          <div key={thought._id} className="card mb-3">
+            <p className="card-header">
+              <Link
+                to={`./profile/${thought.username}`}
+                style={{ fontWeight: 700 }}
+                className="text-light"
+              >
+                {thought.username}
+              </Link>{" "}
+              thought on {thought.createdAt}
             </p>
+            <div className="card-body">
+              <Link to={`/thought/${thought._id}`}>
+                <p>{thought.thoughtText}</p>
+                <p className="mb-0">
+                  Reactions: {thought.reactionsCount} || Click to{" "}
+                  {thought.reactionsCount ? "see" : "start"} the discussion!
+                </p>
+              </Link>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 };
